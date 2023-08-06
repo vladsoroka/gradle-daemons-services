ce163ca2f2c7e8d633e9921cdb6799ffe7ebff81
GitHub Action <action@github.com>
2023-08-06 06:03:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/vladsoroka/gradledaemonsservices/MyAction.kt
+++ src/main/kotlin/com/github/vladsoroka/gradledaemonsservices/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vladsoroka.gradledaemonsservices
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/vladsoroka/gradledaemonsservices/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/vladsoroka/gradledaemonsservices/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vladsoroka.gradledaemonsservices.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
