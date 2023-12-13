65cde8293862faf96d7835306e08165a5a69df46
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2023-12-13 16:14:53 +0400

    Ensure tapi connection is initialized to get daemons info w/o triggering project import

--- fleet-backend-plugin/src/main/kotlin/com/github/vladsoroka/gradle/daemon/services/FleetGradleDaemonsApiProvider.kt
+++ fleet-backend-plugin/src/main/kotlin/com/github/vladsoroka/gradle/daemon/services/FleetGradleDaemonsApiProvider.kt
@@ -9,53 +9,88 @@ import fleet.gradle.daemons.protocol.DaemonInfo
 import fleet.gradle.daemons.protocol.DaemonState
 import fleet.gradle.daemons.protocol.GradleDaemonsApi
 import fleet.util.logging.KLoggers
+import org.gradle.tooling.GradleConnector
+import org.gradle.tooling.model.build.BuildEnvironment
 import org.jetbrains.plugins.gradle.internal.daemon.GradleDaemonServices
+import org.jetbrains.plugins.gradle.settings.GradleSettings
+import java.io.File
+import java.nio.file.Files.createTempDirectory
+import java.util.concurrent.atomic.AtomicBoolean
 
 private val logger by lazy { KLoggers.logger(FleetGradleDaemonsApiProvider::class) }
 
 class FleetGradleDaemonsApiProvider : FleetServiceProvider {
     override fun getServices(workspace: BackendWorkspace): List<FleetServiceDescriptor> {
         return listOf(
-            GradleDaemonsApi::class to {
-                BackendGradleDaemonsApi(workspace)
-            }
+                GradleDaemonsApi::class to {
+                    BackendGradleDaemonsApi(workspace)
+                }
         )
     }
 }
 
 @Suppress("UnstableApiUsage")
 private class BackendGradleDaemonsApi(ws: BackendWorkspace) : WorkspaceAwareApi(ws), GradleDaemonsApi {
+
+    private val initialized = AtomicBoolean()
+
     override suspend fun listDaemons(includeStopped: Boolean): List<DaemonInfo> {
         logger.trace { "Daemons list request..." }
+        ensureInit(ws)
         return GradleDaemonServices.getDaemonsStatus()
-            .filter { includeStopped || it.token != null }
-            .map { it.toDaemonInfo() }
+                .filter { includeStopped || it.token != null }
+                .map { it.toDaemonInfo() }
     }
 
     override suspend fun stopAll(whenIdle: Boolean) {
         if (whenIdle) {
             logger.trace { "Graceful stop all daemons request..." }
             GradleDaemonServices.gracefulStopDaemons()
-        }
-        else {
+        } else {
             logger.trace { "Stop all daemons request..." }
             GradleDaemonServices.stopDaemons()
+        }
+    }
+
+    private fun ensureInit(ws: BackendWorkspace) {
+        if (initialized.get()) return
+        val gradleSettings = ws.projects.firstNotNullOfOrNull { (_, backendProject) ->
+            GradleSettings.getInstance(backendProject.ijProject).takeIf { it.linkedProjectsSettings.isNotEmpty() }
+        } ?: return
+
+        if (initialized.compareAndSet(false, true)) {
+            val gradleProjectSettings = gradleSettings.linkedProjectsSettings.first()
+            val projectConnection = GradleConnector.newConnector()
+                    .forProjectDirectory(File(gradleProjectSettings.externalProjectPath))
+                    .connect()
+            runCatching {
+                val buildEnvironment = projectConnection
+                        .model(BuildEnvironment::class.java)
+                        .setStandardError(OutputWrapper { logger.warn { it } })
+                        .setStandardOutput(OutputWrapper { logger.debug { it } })
+                        .get()
+                logger.debug { "Init succeseed for: ${buildEnvironment.buildIdentifier.rootDir}" }
+            }.onFailure {
+                initialized.set(false)
+                logger.warn(it) { "Unable to init tooling connection. Daemons information might be unavailable till usage of the Gradle tapi." }
+            }
+            projectConnection.close()
         }
     }
 }
 
 private fun org.jetbrains.plugins.gradle.internal.daemon.DaemonState.toDaemonInfo(): DaemonInfo {
     return DaemonInfo(
-        pid = pid,
-        version = version,
-        state = toDaemonState(status),
-        reason = reason,
-        lastBusy = timestamp,
-        daemonExpirationStatus = toDaemonExpirationStatus(daemonExpirationStatus),
-        daemonOpts = daemonOpts ?: emptyList(),
-        javaHome = javaHome?.path,
-        idleTimeout = idleTimeout,
-        registryDir = registryDir?.path
+            pid = pid,
+            version = version,
+            state = toDaemonState(status),
+            reason = reason,
+            lastBusy = timestamp,
+            daemonExpirationStatus = toDaemonExpirationStatus(daemonExpirationStatus),
+            daemonOpts = daemonOpts ?: emptyList(),
+            javaHome = javaHome?.path,
+            idleTimeout = idleTimeout,
+            registryDir = registryDir?.path
     )
 }
 
