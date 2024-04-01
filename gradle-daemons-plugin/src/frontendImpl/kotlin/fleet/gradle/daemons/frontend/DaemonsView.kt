aca56dd95860654c61d1d106d1d073c95519e8e5
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2024-04-01 17:34:36 +0400

    Toolbar vertical aligment

--- gradle-daemons-plugin/src/frontendImpl/kotlin/fleet/gradle/daemons/frontend/DaemonsView.kt
+++ gradle-daemons-plugin/src/frontendImpl/kotlin/fleet/gradle/daemons/frontend/DaemonsView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.DpSize
@@ -91,7 +92,11 @@ internal fun NoriaContext.renderDaemonsView(daemonsViewEntity: DaemonsViewEntity
         }
     }
     Column {
-        Row(modifier = Modifier.padding(8.dp), horizontalArrangement = Arrangement.Center) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
             Spacer(Modifier.width(8.dp))
             Button(text = "Reload", iconKey = IconKeys.Reload, iconSide = IconSide.Left) {
                 tick.update { it + 1 }
